e0786145dec7506e51b073aa71cb792b68b60e51
黄建宇 <565166310@qq.com>
2022-07-10 10:31:29 +0800

    feat: 路由集成

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <AppProvider>
-    <n-space m-50>
-      <n-button>Default</n-button>
-      <n-button type="tertiary"> Tertiary </n-button>
-      <n-button type="primary"> Primary </n-button>
-      <n-button type="info"> Info </n-button>
-      <n-button type="success"> Success </n-button>
-      <n-button type="warning"> Warning </n-button>
-      <n-button type="error"> Error </n-button>
-    </n-space>
+    <router-view v-slot="{ Component }">
+      <component :is="Component" />
+    </router-view>
   </AppProvider>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
 import AppProvider from '@/components/AppProvider/index.vue'
-import { createDiscreteApi, useMessage } from 'naive-ui'
-
-onMounted(() => {
-  const { message } = createDiscreteApi(['message'])
-  // const message = useMessage()
-  // message.success('加载完成，Perfect~')
-  // $loadingBar.start()
-  // setTimeout(() => {
-  //   $loadingBar.finish()
-  message.success('加载完成，Perfect~')
-  // window.$message.success('加载完成，Perfect~')
-  // }, 500)
-})
 </script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  .n-config-provider {
+    height: inherit;
+  }
+}
+</style>
